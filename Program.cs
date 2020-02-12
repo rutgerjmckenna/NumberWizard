7cfb5bcf47a8009570ce2128e54596025a601d77
Rutger <rutgerjmckenna@gmail.com>
2020-02-12 15:08:35 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // This all happens once at the beginning of the run
+    void Start()
+    {
+        //Setting our min and max as variables
+        int max = 1000;
+        int min = 1;
+
+        //Printing to the console with our variables
+        Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Pick a number!");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Checking if someone pushed a key down, and if so, write to the console
+        //GetKeyDown is for every key, and KeyCode designates which key was pressed
+        //End all Debug.Logs with a ;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed.");
+        }
+    }
+}
