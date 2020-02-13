af8f549e3e2ad12924d1e79d626296182c99adcc
Rutger <rutgerjmckenna@gmail.com>
2020-02-13 14:33:50 -0500

    Interpolation, start, OLD COMMENTS IN OLD COMMITS

--- Program.cs
+++ Program.cs
@@ -12,26 +12,21 @@
         int min = 1;
         //Creating new variable as the player's "guess" (hardcoded for now)
         int guess = 500;
-    
-    void Start()
-    {
 
-        //Printing to the console with our variables
+
+    //CREATING A NEW FUNCTION!
+    //Think of void like function in JS (will get into more detail later; really means no return value)
+    //and otherwise we are naming our functions just as we would in JS
+    //This function so far takes in no parameters as of yet but it CAN like in JS
+    void StartGame()
+    {
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number!");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
-        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
-        //Assigning a variable (like on line 41 for example) as itself. This then makes the max
-        //ACTUALLY 1001!
         max = max + 1;
-        //This solution WORKS. Reason being --> this is in our void START, allowing C#
-        //to instantiate this program with max actually equaling 1001 AFTER it does a 
-        //top down run from lines 20-25, where it interpolated the max value (1000) as it was
-        //originally instantiated on line 11.
-        //As C# is a top down reading language, and it runs its void Start FIRST, now
-        //we have the ability to use the 1001 value in our void Update. Victory!
     }
 
     // Update is called once per frame
@@ -42,27 +37,32 @@
         //End all Debug.Logs with a ;
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
             //Now these variables work, and we can Debug.Log our guess (hard coded for now)
             //out to the console
             min = guess;
             //Saying our guess is the maximum and the minimum divided by 2 (this would
             //round down for the number) for example 1000 + 1 / 2 = 500 for us
             guess = (max + min) / 2;
+            Debug.Log("Is it higher or lower than " + guess);
+            //Moved the above Debug.Log to this line to enable it to interpolate the
+            //guess amount, but due to our SCOPE so it doesn't look globally (and instead
+            //within the if statement), we move it lower so our min = guess sets our
+            //guess value
             Debug.Log(guess)
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down Arrow key was pressed.");
             //Same as min, but using max to check other potential input (up or down)
             max = guess;
             //Same as in the if statement to calculate the guess
             guess = (max + min) / 2;
+            //See comment on line 52
+            Debug.Log("Is it higher or lower than " + guess);
             Debug.Log(guess)
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Enter key was pressed.");
+            Debug.Log("I CAN READ YOUR MINDDDDDD");
         }
     }
 }
