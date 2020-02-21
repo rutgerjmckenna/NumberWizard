de9103a5f4a5c02b24ba3186b8ce53db10ca2b2f
Rutger <rutgerjmckenna@gmail.com>
2020-02-20 21:46:24 -0500

    Adding guess text, using the collections, and adding Text Mesh

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿public class NumberWizard : MonoBehaviour
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
 {
     // Start is called before the first frame update
     // This all happens once at the beginning of the run
@@ -13,6 +18,13 @@
         //to be guessed. No longer requires the hard coding.
         [SerializeField] int max
         [SerializeField] int min
+        //Now we are using all of our collections, including TextMeshPro
+        //It would usually just be a normal text box, but since we are
+        //using text mesh pro on our button and input, it is looking for
+        //specifically text mesh pro. Once this is connected on Unity,
+        //it'll know that once guessText is used in the program, it knows
+        //what is input
+        [SerializeField] TextMeshProUGUI guessText;
         //Creating new variable as the player's "guess" (hardcoded for now)
         int guess
     
@@ -29,10 +41,6 @@
     //This function so far takes in no parameters as of yet but it CAN like in JS
     void StartGame()
     {
-        //We keep our max = max + 1 to stay away from the bug we found last time,
-        //but now we are going to take out our hard coded values so we can actually
-        //use Unity and player input with the public voids to get our range.
-        max = max + 1;
 
         //Now since our serialized int max and min are going to be dynamic (lines 14 and 15),
         //we can actually put our guess value here in the void StartGame()
@@ -41,6 +49,15 @@
         guess = (max + min) / 2;
 
         //These values are set in our inspector in the Unity Engine.
+
+        //In our text field, it is looking to display a string, so now we have to
+        //convert the guessText to a string so it can display on the GAME START
+        guessText.text = guess.ToString();
+        
+        //We keep our max = max + 1 to stay away from the bug we found last time,
+        //but now we are going to take out our hard coded values so we can actually
+        //use Unity and player input with the public voids to get our range.
+        max = max + 1;
     }
 
 
